52aa32b982d032491cfcb9f9c99fd1f49a6b8079
augusto <augustocastro@gmail.com>
2024-09-19 18:29:07 -0300

    primer commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './components/PizzeriaIndex'
+import PizzeriaIndex from "./components/PizzeriaIndex";
+
+function App() {
+  return (
+
+    <div className="App">
+      <PizzeriaIndex></PizzeriaIndex>
+    </div>
+  );
+}
+
+export default App;
